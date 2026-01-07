0004a8eb935f274a41108da39a8f12f43bf10179
sreinac <sergiocamiloreinacardenas@MacBook-Pro-de-Sergio.local>
2026-01-07 12:14:36 -0500

    first deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://screinac.github.io',
+    base: '/lifevisor-website',
+
     vite: {
         plugins: [tailwindcss()],
     },
